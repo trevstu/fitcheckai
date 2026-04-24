4914c7c8dc7bda8812cc567cf1474e84700a7ef3
Trevor Stuart <trevor@saintstu.com>
2026-04-24 10:50:21 -0400

    Make FitCheck standalone homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
 import FitCheck from './FitCheck.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/fitcheck" element={<FitCheck />} />
-      </Routes>
-    </BrowserRouter>
+    <FitCheck />
   </StrictMode>,
 )
